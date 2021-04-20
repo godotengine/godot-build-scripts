ba0aa611d59508a0c8823d98bdda87a8b6522cbf
Rémi Verschelde <remi@godotengine.org>
2021-04-20 12:19:43 +0200

    Mono glue: Use dummy audio driver to silence errors

--- build-mono-glue/build.sh
+++ build-mono-glue/build.sh
@@ -24,11 +24,11 @@ if [ "${MONO}" == "1" ]; then
   ${SCONS} platform=x11 bits=64 ${OPTIONS} target=release_debug tools=yes module_mono_enabled=yes mono_glue=no
 
   rm -rf /root/mono-glue/*
-  xvfb-run bin/godot.x11.opt.tools.64.mono --generate-mono-glue /root/mono-glue || /bin/true
+  xvfb-run bin/godot.x11.opt.tools.64.mono --audio-driver Dummy --generate-mono-glue /root/mono-glue || /bin/true
 
   # Build and copy CIL that we'll need for Ubuntu 14.04 Linux builds
   # to workaround Mono MSBuild segfault in our containers.
-  xvfb-run bin/godot.x11.opt.tools.64.mono --generate-mono-glue modules/mono/glue || /bin/true
+  xvfb-run bin/godot.x11.opt.tools.64.mono --audio-driver Dummy --generate-mono-glue modules/mono/glue || /bin/true
   ${SCONS} platform=x11 bits=64 ${OPTIONS} target=release_debug tools=yes module_mono_enabled=yes mono_glue=yes build_cil=yes
 
   mkdir /root/mono-glue/cil
