96b8f89a2262230323693a4dc53a07d10f3548a1
Rémi Verschelde <remi@godotengine.org>
2023-11-01 13:50:57 +0100

    Linux: Update to new SDK with GCC 13, add ARM64 and ARM32

--- build-linux/build.sh
+++ build-linux/build.sh
@@ -14,9 +14,6 @@ mkdir godot
 cd godot
 tar xf /root/godot.tar.gz --strip-components=1
 
-# pkg-config wrongly points to lib instead of lib64 for arch-dependent header.
-sed -i ${GODOT_SDK_LINUX_X86_64}/x86_64-godot-linux-gnu/sysroot/usr/lib/pkgconfig/dbus-1.pc -e "s@/lib@/lib64@g"
-
 # Classical
 
 if [ "${CLASSICAL}" == "1" ]; then
@@ -35,7 +32,7 @@ if [ "${CLASSICAL}" == "1" ]; then
   cp -rvp bin/* /root/out/x86_64/templates
   rm -rf bin
 
-  export PATH="${GODOT_SDK_LINUX_X86}/bin:${BASE_PATH}"
+  export PATH="${GODOT_SDK_LINUX_X86_32}/bin:${BASE_PATH}"
 
   $SCONS platform=linuxbsd arch=x86_32 $OPTIONS target=editor
   mkdir -p /root/out/x86_32/tools
@@ -46,6 +43,32 @@ if [ "${CLASSICAL}" == "1" ]; then
   $SCONS platform=linuxbsd arch=x86_32 $OPTIONS target=template_release
   mkdir -p /root/out/x86_32/templates
   cp -rvp bin/* /root/out/x86_32/templates
+  rm -rf bin
+
+  export PATH="${GODOT_SDK_LINUX_ARM64}/bin:${BASE_PATH}"
+
+  $SCONS platform=linuxbsd arch=arm64 $OPTIONS target=editor
+  mkdir -p /root/out/arm64/tools
+  cp -rvp bin/* /root/out/arm64/tools
+  rm -rf bin
+
+  $SCONS platform=linuxbsd arch=arm64 $OPTIONS target=template_debug
+  $SCONS platform=linuxbsd arch=arm64 $OPTIONS target=template_release
+  mkdir -p /root/out/arm64/templates
+  cp -rvp bin/* /root/out/arm64/templates
+  rm -rf bin
+
+  export PATH="${GODOT_SDK_LINUX_ARM32}/bin:${BASE_PATH}"
+
+  $SCONS platform=linuxbsd arch=arm32 $OPTIONS target=editor
+  mkdir -p /root/out/arm32/tools
+  cp -rvp bin/* /root/out/arm32/tools
+  rm -rf bin
+
+  $SCONS platform=linuxbsd arch=arm32 $OPTIONS target=template_debug
+  $SCONS platform=linuxbsd arch=arm32 $OPTIONS target=template_release
+  mkdir -p /root/out/arm32/templates
+  cp -rvp bin/* /root/out/arm32/templates
   rm -rf bin
 fi
 
@@ -71,7 +94,7 @@ if [ "${MONO}" == "1" ]; then
   cp -rvp bin/* /root/out/x86_64/templates-mono
   rm -rf bin
 
-  export PATH="${GODOT_SDK_LINUX_X86}/bin:${BASE_PATH}"
+  export PATH="${GODOT_SDK_LINUX_X86_32}/bin:${BASE_PATH}"
 
   $SCONS platform=linuxbsd arch=x86_32 $OPTIONS $OPTIONS_MONO target=editor
   ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin --godot-platform=linuxbsd
@@ -83,6 +106,34 @@ if [ "${MONO}" == "1" ]; then
   $SCONS platform=linuxbsd arch=x86_32 $OPTIONS $OPTIONS_MONO target=template_release
   mkdir -p /root/out/x86_32/templates-mono
   cp -rvp bin/* /root/out/x86_32/templates-mono
+  rm -rf bin
+
+  export PATH="${GODOT_SDK_LINUX_ARM64}/bin:${BASE_PATH}"
+
+  $SCONS platform=linuxbsd arch=arm64 $OPTIONS $OPTIONS_MONO target=editor
+  ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin --godot-platform=linuxbsd
+  mkdir -p /root/out/arm64/tools-mono
+  cp -rvp bin/* /root/out/arm64/tools-mono
+  rm -rf bin
+
+  $SCONS platform=linuxbsd arch=arm64 $OPTIONS $OPTIONS_MONO target=template_debug
+  $SCONS platform=linuxbsd arch=arm64 $OPTIONS $OPTIONS_MONO target=template_release
+  mkdir -p /root/out/arm64/templates-mono
+  cp -rvp bin/* /root/out/arm64/templates-mono
+  rm -rf bin
+
+  export PATH="${GODOT_SDK_LINUX_ARM32}/bin:${BASE_PATH}"
+
+  $SCONS platform=linuxbsd arch=arm32 $OPTIONS $OPTIONS_MONO target=editor
+  ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin --godot-platform=linuxbsd
+  mkdir -p /root/out/arm32/tools-mono
+  cp -rvp bin/* /root/out/arm32/tools-mono
+  rm -rf bin
+
+  $SCONS platform=linuxbsd arch=arm32 $OPTIONS $OPTIONS_MONO target=template_debug
+  $SCONS platform=linuxbsd arch=arm32 $OPTIONS $OPTIONS_MONO target=template_release
+  mkdir -p /root/out/arm32/templates-mono
+  cp -rvp bin/* /root/out/arm32/templates-mono
   rm -rf bin
 fi
 
