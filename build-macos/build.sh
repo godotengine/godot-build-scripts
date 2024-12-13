992b60f37be273691643e6406fc6883fd4d72bb4
Rémi Verschelde <remi@godotengine.org>
2024-12-13 16:05:26 +0100

    Add support for releasing debug symbols for desktop platforms

--- build-macos/build.sh
+++ build-macos/build.sh
@@ -16,7 +16,7 @@ MACOS_SDK_PATH="${MACOS_SDK_PATH:-}"
 EXTRA_LINK_FLAGS="${EXTRA_LINK_FLAGS:-}"
 
 export SCONS="scons -j${NUM_CORES} verbose=yes warnings=no progress=no redirect_build_objects=no"
-export OPTIONS="production=yes use_volk=no vulkan_sdk_path=/root/moltenvk angle_libs=/root/angle accesskit_sdk_path=/root/accesskit/accesskit-c SWIFT_COMPILER=${SWIFT_COMPILER} MACOS_SDK_PATH=${MACOS_SDK_PATH}"
+export OPTIONS="production=yes debug_symbols=yes separate_debug_symbols=no debug_paths_relative=yes use_volk=no vulkan_sdk_path=/root/moltenvk angle_libs=/root/angle accesskit_sdk_path=/root/accesskit/accesskit-c SWIFT_COMPILER=${SWIFT_COMPILER} MACOS_SDK_PATH=${MACOS_SDK_PATH}"
 export OPTIONS_MONO="module_mono_enabled=yes"
 export OPTIONS_DOTNET="module_dotnet_enabled=yes"
 export TERM=xterm
@@ -30,6 +30,13 @@ mkdir godot
 cd godot
 tar xf /root/godot.tar.gz --strip-components=1
 
+lipo_and_extract_dsym() {
+  [ "$2" == "mono" ] && mono=".mono"
+  llvm-lipo -create bin/$1.x86_64$mono bin/$1.arm64$mono -output bin/$1.universal$mono
+  dsymutil bin/$1.universal$mono -o bin/$1.universal$mono.dSYM
+  llvm-strip bin/$1.universal$mono
+}
+
 # Classical
 
 if [ "${CLASSICAL}" == "1" ]; then
@@ -37,7 +44,7 @@ if [ "${CLASSICAL}" == "1" ]; then
 
   run_scons platform=macos $OPTIONS arch=x86_64 target=editor
   run_scons platform=macos $OPTIONS arch=arm64 target=editor
-  lipo -create bin/godot.macos.editor.x86_64 bin/godot.macos.editor.arm64 -output bin/godot.macos.editor.universal
+  lipo_and_extract_dsym godot.macos.editor
 
   mkdir -p /root/out/tools
   cp -rvp bin/* /root/out/tools
@@ -48,7 +55,7 @@ if [ "${CLASSICAL}" == "1" ]; then
     export BUILD_NAME="steam"
     run_scons platform=macos arch=x86_64 $OPTIONS target=editor steamapi=yes
     run_scons platform=macos arch=arm64 $OPTIONS target=editor steamapi=yes
-    lipo -create bin/godot.macos.editor.x86_64 bin/godot.macos.editor.arm64 -output bin/godot.macos.editor.universal
+    lipo_and_extract_dsym godot.macos.editor
 
     mkdir -p /root/out/steam
     cp -rvp bin/* /root/out/steam
@@ -58,10 +65,10 @@ if [ "${CLASSICAL}" == "1" ]; then
 
   run_scons platform=macos $OPTIONS arch=x86_64 target=template_debug
   run_scons platform=macos $OPTIONS arch=arm64 target=template_debug
-  lipo -create bin/godot.macos.template_debug.x86_64 bin/godot.macos.template_debug.arm64 -output bin/godot.macos.template_debug.universal
+  lipo_and_extract_dsym godot.macos.template_debug
   run_scons platform=macos $OPTIONS arch=x86_64 target=template_release
   run_scons platform=macos $OPTIONS arch=arm64 target=template_release
-  lipo -create bin/godot.macos.template_release.x86_64 bin/godot.macos.template_release.arm64 -output bin/godot.macos.template_release.universal
+  lipo_and_extract_dsym godot.macos.template_release
 
   mkdir -p /root/out/templates
   cp -rvp bin/* /root/out/templates
@@ -78,7 +85,7 @@ if [ "${MONO}" == "1" ]; then
 
   run_scons platform=macos $OPTIONS $OPTIONS_MONO arch=x86_64 target=editor
   run_scons platform=macos $OPTIONS $OPTIONS_MONO arch=arm64 target=editor
-  lipo -create bin/godot.macos.editor.x86_64.mono bin/godot.macos.editor.arm64.mono -output bin/godot.macos.editor.universal.mono
+  lipo_and_extract_dsym godot.macos.editor mono
   ./modules/mono/build_scripts/build_assemblies.py --godot-output-dir=./bin --godot-platform=macos
 
   mkdir -p /root/out/tools-mono
@@ -87,10 +94,10 @@ if [ "${MONO}" == "1" ]; then
 
   run_scons platform=macos $OPTIONS $OPTIONS_MONO arch=x86_64 target=template_debug
   run_scons platform=macos $OPTIONS $OPTIONS_MONO arch=arm64 target=template_debug
-  lipo -create bin/godot.macos.template_debug.x86_64.mono bin/godot.macos.template_debug.arm64.mono -output bin/godot.macos.template_debug.universal.mono
+  lipo_and_extract_dsym godot.macos.template_debug mono
   run_scons platform=macos $OPTIONS $OPTIONS_MONO arch=x86_64 target=template_release
   run_scons platform=macos $OPTIONS $OPTIONS_MONO arch=arm64 target=template_release
-  lipo -create bin/godot.macos.template_release.x86_64.mono bin/godot.macos.template_release.arm64.mono -output bin/godot.macos.template_release.universal.mono
+  lipo_and_extract_dsym godot.macos.template_release mono
 
   mkdir -p /root/out/templates-mono
   cp -rvp bin/* /root/out/templates-mono
@@ -104,7 +111,7 @@ if [ "${DOTNET}" == "1" ]; then
 
   run_scons platform=macos $OPTIONS $OPTIONS_DOTNET arch=x86_64 target=editor
   run_scons platform=macos $OPTIONS $OPTIONS_DOTNET arch=arm64 target=editor
-  lipo -create bin/godot.macos.editor.x86_64.dotnet bin/godot.macos.editor.arm64.dotnet -output bin/godot.macos.editor.universal.dotnet
+  lipo_and_extract_dsym godot.macos.editor dotnet
 
   mkdir -p /root/out/tools-dotnet
   cp -rvp bin/* /root/out/tools-dotnet
@@ -112,10 +119,10 @@ if [ "${DOTNET}" == "1" ]; then
 
   run_scons platform=macos $OPTIONS $OPTIONS_DOTNET arch=x86_64 target=template_debug
   run_scons platform=macos $OPTIONS $OPTIONS_DOTNET arch=arm64 target=template_debug
-  lipo -create bin/godot.macos.template_debug.x86_64.dotnet bin/godot.macos.template_debug.arm64.dotnet -output bin/godot.macos.template_debug.universal.dotnet
+  lipo_and_extract_dsym godot.macos.template_debug dotnet
   run_scons platform=macos $OPTIONS $OPTIONS_DOTNET arch=x86_64 target=template_release
   run_scons platform=macos $OPTIONS $OPTIONS_DOTNET arch=arm64 target=template_release
-  lipo -create bin/godot.macos.template_release.x86_64.dotnet bin/godot.macos.template_release.arm64.dotnet -output bin/godot.macos.template_release.universal.dotnet
+  lipo_and_extract_dsym godot.macos.template_release dotnet
 
   mkdir -p /root/out/templates-dotnet
   cp -rvp bin/* /root/out/templates-dotnet
