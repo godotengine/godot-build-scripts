d74f1377991669399d96742494e13bb84470c47a
Rémi Verschelde <remi@godotengine.org>
2025-10-10 14:05:40 +0200

    Move JSON key location to config.sh

--- build-android/upload-playstore.sh
+++ build-android/upload-playstore.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 BASEDIR="$(pwd)"
+
+source ${BASEDIR}/config.sh
+
 VENV_DIR="${BASEDIR}/venv"
 PYTHON_SCRIPT="${BASEDIR}/build-android/playstore_upload_script.py"
 AAB_FILE="${BASEDIR}/out/android/tools/android_editor.aab"
 NDS_FILE="${BASEDIR}/out/android/tools/android_editor_native_debug_symbols.zip"
-JSON_KEY_FILE="${BASEDIR}/playstore_key.json"
+JSON_KEY_FILE="${BASEDIR}/${GODOT_ANDROID_UPLOAD_JSON_KEY}"
 
 echo "Creating virtual environment"
 rm -rf "$VENV_DIR"
