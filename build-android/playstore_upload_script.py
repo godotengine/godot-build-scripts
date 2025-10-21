257bffd2aa79edabc6dd21ac74b2ef5c3401a312
Anish Kumar <mishragames@gmail.com>
2025-10-21 13:49:21 +0530

    Android Editor: Add release url to dev/beta/rc notes on Play Store

--- build-android/playstore_upload_script.py
+++ build-android/playstore_upload_script.py
@@ -10,17 +10,34 @@ def main(aab_path, nds_path, key_path, version_name):
     major = version_parts[0]
     minor = version_parts[1]
     patch = int(version_parts[2]) if len(version_parts) > 2 else 0
+    channel = version_name.split("-")[1]
 
     release_note = f"Godot Engine {version_name} has arrived!\nNote: This is a pre-release piece of software so be sure to make backups."
     track = "alpha"
 
-    if version_name.endswith("stable"):
+    if "stable" in channel:
         if patch == 0:
             release_url = f"https://godotengine.org/releases/{major}.{minor}/"
         else:
             release_url = f"https://godotengine.org/article/maintenance-release-godot-{major}-{minor}-{patch}/"
         release_note = f"Godot Engine {version_name} has arrived!\nRelease page: {release_url}"
         track = "beta"
+    elif "rc" in channel:
+        channel_url = channel.replace("rc", "rc-")
+        if patch == 0:
+            release_url = f"https://godotengine.org/article/release-candidate-godot-{major}-{minor}-{channel_url}/"
+        else:
+            release_url = f"https://godotengine.org/article/release-candidate-godot-{major}-{minor}-{patch}-{channel_url}/"
+        release_note += f"\nRelease page: {release_url}"
+    else:
+        # No need to handle patch versions here: maintenance releases go straight to RC and stable.
+        # There are no 4.5.1-dev or 4.5.1-beta builds.
+        if "beta" in channel:
+            channel_url = channel.replace("beta", "beta-")
+        else:
+            channel_url = channel.replace("dev", "dev-")
+        release_url = f"https://godotengine.org/article/dev-snapshot-godot-{major}-{minor}-{channel_url}/"
+        release_note += f"\nRelease page: {release_url}"
 
     scopes = ["https://www.googleapis.com/auth/androidpublisher"]
     credentials = service_account.Credentials.from_service_account_file(key_path, scopes=scopes)
